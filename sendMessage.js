4ea3f4d67975e84e8c13e8a926d7e4f58fddbdcb
parag poddar <paragpoddar01@gmail.com>
2019-01-10 16:29:46 +0530

    Update

--- sendMessage.js
+++ sendMessage.js
@@ -5,13 +5,10 @@ const DDB = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 const { TABLE_NAME } = process.env;
 
 exports.handler = async (event) => {
-    console.log("Event: ", JSON.stringify(event));
     if (event.requestContext.eventType === "MESSAGE") {
         let connectionData;
         try {
-            console.log("Table name ", TABLE_NAME);
             connectionData = await DDB.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
-            console.log("Connection data ", connectionData);
         } catch (e) {
             return { statusCode: 500, body: e.stack };
         }
@@ -20,15 +17,11 @@ exports.handler = async (event) => {
             endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
         });
         const postData = JSON.parse(event.body).data;
-        console.log("Post data ", postData);
         const postCalls = connectionData.Items.map(async ({ connectionId }) => {
             try {
-                console.log("Connection id ", connectionId);
                 await apiManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
-                console.log("Post to connection");
             } catch (e) {
                 if (e.statusCode === 410) {
-                    console.log(`Found stale connection, deleting ${connectionId}`);
                     await DDB.deleteItem({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
                 } else {
                     throw e;
